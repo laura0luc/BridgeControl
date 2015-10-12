49a2040461d36ec6dbb80265344ce9061b867a54
Laura SaÃÅnchez <laura0luc@gmail.com>
2015-10-12 18:49:24 -0500

    Initial Commit

---
+++ Bridge Control/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Bridge Control
+//
+//  Created by LAURA LUCRECIA SANCHEZ PADILLA on 12/10/15.
+//  Copyright © 2015 LAURA LUCRECIA SANCHEZ PADILLA. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Bridge Control/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Bridge Control
+//
+//  Created by LAURA LUCRECIA SANCHEZ PADILLA on 12/10/15.
+//  Copyright © 2015 LAURA LUCRECIA SANCHEZ PADILLA. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
